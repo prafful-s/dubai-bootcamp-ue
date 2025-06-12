5fb65e64416a5c4230fbf692e1c928b9ecaf5512
prafful-s <151611931+prafful-s@users.noreply.github.com>
2025-06-12 09:55:02 +0530

    Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -588,6 +588,11 @@ async function loadBlock(block) {
  */
 function decorateBlock(block) {
   const shortBlockName = block.classList[0];
+  if(document.getElementsByClassName('hero').length > 0 && document.getElementsByClassName('hero')[0].children.length == 3){
+    const className = document.getElementsByClassName('hero')[0].children[2].children[0].innerHTML;
+    block.classList.add(className);
+    document.getElementsByClassName('hero')[0].children[2].remove();
+  }
   if (shortBlockName && !block.dataset.blockStatus) {
     block.classList.add('block');
     block.dataset.blockName = shortBlockName;
